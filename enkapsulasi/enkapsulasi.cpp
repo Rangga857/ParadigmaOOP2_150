cfdfd663a5d58a729b0ce54714fe73c00651b44e
Rangga857 <azharrangga28@gmail.com>
2023-05-29 12:54:52 +0700

    membuat int main

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -3,5 +3,28 @@ using namespace std;
 
 class remoteLampu {
 private:
-	string saklarNo[10]
+	string saklarNo[10];
+public :
+	void setSaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string getSaklarNo(int i) {
+		return saklarNo[i];
+	}
 };
+
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Ruang tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Kamar Tidue");
+	lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getSaklarNo(0) << endl;
+	cout << lampuRumah.getSaklarNo(1) << endl;
+	cout << lampuRumah.getSaklarNo(2) << endl;
+	cout << lampuRumah.getSaklarNo(3) << endl;
+
+	return 0;
+}
